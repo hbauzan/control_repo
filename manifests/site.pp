591e40d18cb69abe131b67afccaf8371c01ba4fd
hbauzan <61513414+hbauzan@users.noreply.github.com>
2021-10-21 17:03:58 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,2 @@
 node default { 
-  file {'/root/README':
-    ensure  => file,
-    content => 'Aca se debe poner un hola mundo o alguna bolduez de esa pero no voy a poner nada',
-    owner   => 'root',
-  }
 }
