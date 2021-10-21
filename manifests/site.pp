282e7c062ed16267b040951047c764a680dbf798
hbauzan <61513414+hbauzan@users.noreply.github.com>
2021-10-21 16:23:41 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default { 
   file {'/root/README':
     ensure => file,
-    content => 'Aca se debe poner un hola mundo o alguna bolduez de esa pero no voy a poner nada'.
+    content => 'Aca se debe poner un hola mundo o alguna bolduez de esa pero no voy a poner nada',
   }
 }
